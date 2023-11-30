1224356bc3855ec6935d9b5c557ec3f11cd078e1
cgil <lao.tseu.is.alive@gmail.com>
2023-11-30 08:36:18 +0100

    format

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -3,10 +3,9 @@ use std::env;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let num_args = args.len();
-    if  num_args > 1 {
+    if num_args > 1 {
         println!("Hello {} !", args[1])
     } else {
         println!("Hello, world!");
     }
-
 }
